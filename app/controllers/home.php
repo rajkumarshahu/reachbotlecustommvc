53429fcf330295f74ba2b593c1dea3e2ae761ce5
Rajkumar <rajkumarshahu@gmail.com>
2017-12-26 19:44:59 -0500

    changes before adding design

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -3,7 +3,10 @@ if(file_exists($config['MODELS'].$controller.$config['EXT_PHP'])){
     include $config['MODELS'].$controller.$config['EXT_PHP'];
 }
 
-if($view == "index"){
-
-    print "You are in the index view inside of the controller";
+if(file_exists($config['VIEWS'].$controller.$config['EXT_PHTML'])){
+    include $config['VIEWS'].$controller.$config['EXT_PHTML'];
 }
+
+/*if($view == "index"){
+    print "You are in the index view inside of the controller";
+}*/
