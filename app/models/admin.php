e27a866f3dedfe11faedbe5f9c555c05bc57ed70
Rajkumar <rajkumarshahu@gmail.com>
2018-01-01 20:21:29 -0500

    final

--- app/models/admin.php
+++ app/models/admin.php
@@ -1,6 +1,6 @@
 <?php
 function get_all_orders($db){
-    $sqlOrders = "SELECT * FROM `orders` LEFT JOIN `users` ON orders.user_id = users.id order by orders.`id` desc";
+    $sqlOrders = "SELECT *,orders.id as order_id FROM `orders` LEFT JOIN `users` ON orders.user_id = users.id order by orders.`id` desc";
     $resultOrders = $db->query($sqlOrders);
 
     $order_information = null;
@@ -9,7 +9,7 @@ function get_all_orders($db){
         $orders = $resultOrders->fetchAll(PDO::FETCH_ASSOC);
         $i=0;
         foreach ($orders as $order) {
-            $order_id = $order['id'];
+            $order_id = $order['order_id'];
             $order_information[$i]['order'] = $order;
             $sqlUserProducts = "SELECT * FROM `user_products` LEFT JOIN `items` ON `user_products`.`item_id` = `items`.`id` WHERE `order_id`='$order_id'";
             $resultUserProducts = $db->query($sqlUserProducts);
