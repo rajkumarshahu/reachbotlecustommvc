a5462c7136b055325171c08ea1bda234c29d9e88
Rajkumar <rajkumarshahu@gmail.com>
2018-01-01 20:07:10 -0500

    Reach bottle admin page complete

---
+++ app/models/admin.php
@@ -0,0 +1,22 @@
+<?php
+function get_all_orders($db){
+    $sqlOrders = "SELECT * FROM `orders` LEFT JOIN `users` ON orders.user_id = users.id order by orders.`id` desc";
+    $resultOrders = $db->query($sqlOrders);
+
+    $order_information = null;
+
+    if($resultOrders->rowCount()>0) {
+        $orders = $resultOrders->fetchAll(PDO::FETCH_ASSOC);
+        $i=0;
+        foreach ($orders as $order) {
+            $order_id = $order['id'];
+            $order_information[$i]['order'] = $order;
+            $sqlUserProducts = "SELECT * FROM `user_products` LEFT JOIN `items` ON `user_products`.`item_id` = `items`.`id` WHERE `order_id`='$order_id'";
+            $resultUserProducts = $db->query($sqlUserProducts);
+            $order_information[$i]['items'] = $resultUserProducts->fetchAll(PDO::FETCH_ASSOC);
+            $i++;
+        }
+    }
+    return $order_information;
+}
+$order_information = get_all_orders($db);
