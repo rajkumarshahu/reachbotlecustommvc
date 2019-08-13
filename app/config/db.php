b085bdab756901d70c46967ec9c76274b4717b66
Raj <rajkumarshahu@gmail.com>
2019-08-12 20:38:34 -0400

    Update db.php

--- app/config/db.php
+++ app/config/db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO("mysql:host=127.0.0.1;dbname=reachbottle;", "root", ''); #localhost
+    $db = new PDO("mysql:host=remotemysql.com;dbname=SuvQ9uNgmQ;", "SuvQ9uNgmQ", '2lYGi6TCmw'); #localhost
 #    $db = new PDO("mysql:host=127.0.0.1;dbname=f7933500_comp120_fri_10am;", "f7933500_raj", 'ZI==GB!GlMdO'); #gblearn
     //$db = new PDO("mysql:host=localhost;dbname=f7team8_mydatabase;", "f7team8_admin", '1JrlzF,a?+Z.'); #gblearn
 }
