9d4fa0354382cbf1c30421a7c01d779462bbbf2d
Raj <rajkumarshahu@gmail.com>
2019-08-12 20:48:06 -0400

    Checking

--- app/controllers/controller.php
+++ app/controllers/controller.php
@@ -1,4 +1,9 @@
 <?php
+
+print_r($config);
+
+print_r($controller);
+
 if(file_exists($config['MODELS'].$controller.$config['EXT_PHP'])){
     include $config['MODELS'].$controller.$config['EXT_PHP'];
 }
